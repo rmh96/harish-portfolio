bf7ba9e71872337975b9f9b88336e5546a0b1ca5
rmh96 <100729581+rmh96@users.noreply.github.com>
2023-12-30 20:06:53 +0530

    Initial commit with main section completed code for portfolio

---
+++ index.js
@@ -0,0 +1,107 @@
+//Cursor Effect
+// const circles = document.querySelectorAll(".circle");
+// const coords = { x: 0, y: 0 };
+
+// circles.forEach((circle) => {
+//   circle.x = 0;
+//   circle.y = 0;
+// });
+
+// const animateCursor = () => {
+//   let x = coords.x;
+//   let y = coords.y;
+
+//   circles.forEach((circle, index) => {
+//     circle.style.left = x - 12 + "px";
+//     circle.style.top = y - 12 + "px";
+//     circle.x = x;
+//     circle.y = y;
+
+//     const nextCircle = circle[index + 1] || circle[0];
+//     x += (nextCircle.x - x) * 0.3;
+//     y += (nextCircle.y - y) * 0.3;
+//   });
+// };
+
+// window.addEventListener("mousemove", (e) => {
+//   coords.x = e.clientX;
+//   coords.y = e.clientY;
+//   animateCursor();
+// });
+
+//nav bg-color change dynamically as per section
+const nav = document.querySelector("nav");
+const main = document.querySelector("#main");
+const skills = document.querySelector("#skills");
+const about = document.querySelector("#about");
+const exp = document.querySelector("#exp");
+
+const getBgStyleValue = (div) => {
+  return window.getComputedStyle(div).backgroundColor;
+};
+
+window.addEventListener("scroll", () => {
+  const scrollY = window.scrollY;
+  const commonHeightOfSections = main.offsetHeight;
+
+  if (scrollY <= main.offsetTop + commonHeightOfSections) {
+    nav.style.backgroundColor = getBgStyleValue(main);
+  }
+  if (
+    scrollY >= about.offsetTop &&
+    scrollY < about.offsetTop + commonHeightOfSections
+  ) {
+    nav.style.backgroundColor = getBgStyleValue(about);
+  }
+  if (
+    scrollY >= skills.offsetTop &&
+    scrollY < skills.offsetTop + commonHeightOfSections
+  ) {
+    nav.style.backgroundColor = getBgStyleValue(skills);
+  }
+  if (scrollY >= skills.offsetTop + commonHeightOfSections) {
+    nav.style.backgroundColor = getBgStyleValue(exp);
+  }
+});
+
+// smooth page scroll logics
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", (event) => {
+    event.preventDefault();
+    const targetId = anchor.getAttribute("href");
+    const targetEle = document.querySelector(targetId);
+    if (targetEle) {
+      targetEle.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  });
+});
+
+//download resume btn click dummy effect
+const btnLoading = document.querySelector("#btn-loading");
+btnLoading.addEventListener("click", () => {
+  btnLoading.classList.add("btn-loading");
+  btnLoading.addEventListener("animationend", () => {
+    btnLoading.classList.remove("btn-loading");
+  });
+});
+
+//translate x effect main
+const parent = document.getElementById("main-con");
+const children = parent.children;
+
+Array.from(children).forEach((child, index) => {
+  const delay =
+    index === 0 ? 0.5 : index === 1 ? 1.5 : index === 1.5 ? 1.8 : 3.2;
+  child.style.animation = `slidingX 1s both ${delay}s`;
+});
+
+//scroll watcher top logic based on nav
+const scrollTopValueSet = () => {
+  const navHeight = document.querySelector("nav").offsetHeight;
+  const scrollWatcher = document.querySelector(".scroll-watcher");
+  scrollWatcher.style.top = navHeight + "px";
+};
+scrollTopValueSet();
+window.addEventListener("resize", scrollTopValueSet);
